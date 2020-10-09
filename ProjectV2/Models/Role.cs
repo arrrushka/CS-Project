5f4c6299b0ebe622ff7673af2d04c6da11f0b627
Yedil Dinmukhamed <54952217+dxrk23@users.noreply.github.com>
2020-10-09 16:21:37 +0600

    Roles

---
+++ ProjectV2/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectV2.Models
+{
+    public class Role
+    {
+        public const string Teacher = "Teacher";
+        public const string Student = "Student";
+    }
+}
